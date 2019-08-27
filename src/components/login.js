ae889c48fe668555974fd93e8ecd7e3ae685af12
Benard Obinna <bernardobinna2@gmail.com>
2019-08-27 13:01:37 +0100

    setup react hooks

---
+++ src/components/login.js
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+
+export default function Login(props) {
+  const [userName, setUser] = useState("");
+  const [userPassword, setPassword] = useState("");
+  const submit = e => {
+    e.preventDefault();
+    console.log(e);
+    console.log(e.target);
+    console.log(userName);
+    console.log(userPassword);
+    console.log(props);
+  };
+  return (
+    <div>
+      <p>E-Dashboard</p>
+      <form onSubmit={submit}>
+        <div>
+          <label htmlFor="username"> Username </label>
+          <input
+            className="input"
+            name="username"
+            type="text"
+            placeholder="Username"
+            onChange={e => setUser(e.target.value)}
+          />
+        </div>
+        <div>
+          <label htmlFor="password"> Password </label>
+          <input
+            className="input"
+            type="password"
+            name="password"
+            placeholder="Password"
+            onChange={e => setPassword(e.target.value)}
+          />
+        </div>
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+  );
+}
