4d5dcdd6231ba0cd698a68fbd24aef26fb9e3ee0
Benard Obinna <bernardobinna2@gmail.com>
2019-08-28 02:04:03 +0100

    added dahsboard and dashbody components for fleshing out

---
+++ src/components/dashBody.js
@@ -0,0 +1,16 @@
+import React, { useState } from "react";
+
+export default function dashBody() {
+  return (
+    <div>
+      <div className="grid-container-heading">
+        <h3>Product Code</h3>
+        <h3>Product</h3>
+        <h3>Special Insrtuctions</h3>
+        <h3>Status</h3>
+        <h3>Date</h3>
+        <h3>Quantity</h3>
+      </div>
+    </div>
+  );
+}
--- src/components/dashboard.js
+++ src/components/dashboard.js
@@ -1,9 +1,26 @@
 import React from "react";
+import DashBody from "./dashBody";
 
 export default function dashboard() {
   return (
     <div>
-      <p>ndfjdn </p>
+      <div className="grid-container">
+        <aside className="grid-item menu">
+          <h2>Overview</h2>
+          <h3>Orders Today</h3>
+          <strong>254</strong>
+          <h3>Pending Orders</h3>
+          <strong>09</strong>
+          <h3>Delivered Orders </h3>
+          <strong>125</strong>
+          <h3>Cancelled orders</h3>
+          <strong>20</strong>
+        </aside>
+        <aside className="grid-item dash">
+          {/* <p>a1</p> */}
+          <DashBody />
+        </aside>
+      </div>
     </div>
   );
 }
