3aa1d529419953b302b76d4c9e41335cb912fef7
Benard Obinna <bernardobinna2@gmail.com>
2019-08-28 02:15:43 +0100

    styled dashbody a bit

--- src/components/dashBody.js
+++ src/components/dashBody.js
@@ -2,14 +2,14 @@ import React, { useState } from "react";
 
 export default function dashBody() {
   return (
-    <div>
+    <div className="dashbody">
       <div className="grid-container-heading">
-        <h3>Product Code</h3>
-        <h3>Product</h3>
-        <h3>Special Insrtuctions</h3>
-        <h3>Status</h3>
-        <h3>Date</h3>
-        <h3>Quantity</h3>
+        <h3 className="grid-item-heading">Product Code</h3>
+        <h3 className="grid-item-heading">Product</h3>
+        <h3 className="grid-item-heading">Special Insrtuctions</h3>
+        <h3 className="grid-item-heading">Status</h3>
+        <h3 className="grid-item-heading">Date</h3>
+        <h3 className="grid-item-heading">Quantity</h3>
       </div>
     </div>
   );
